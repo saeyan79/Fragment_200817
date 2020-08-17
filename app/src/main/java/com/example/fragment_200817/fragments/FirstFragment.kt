62391ea79568cd108c29079ebcc1f232698fd670
박새 <yannn7979@gmail.com>
2020-08-17 22:08:57 +0900

    프래그먼트 xml/코틀린파일 연결   - onCreateView 내용 갈아 엎기

--- app/src/main/java/com/example/fragment_200817/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragment_200817/fragments/FirstFragment.kt
@@ -1,9 +1,19 @@
 package com.example.fragment_200817.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fragment_200817.R
 
 class FirstFragment : Fragment() {
-
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
 
 }
