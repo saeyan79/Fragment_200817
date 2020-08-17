5b4b646ee43a0212a180e1361f608a6d385665de
박새 <yannn7979@gmail.com>
2020-08-17 22:14:42 +0900

    프래그먼트의 버튼 이벤트 처리

--- app/src/main/java/com/example/fragment_200817/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragment_200817/fragments/FirstFragment.kt
@@ -6,14 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fragment_200817.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // 프래그먼트에 있는 버튼이 눌리면 -=> 텍스트뷰 문구 변경
+        changeTextBtn.setOnClickListener {
+            contentTxt.text = "프래그먼트에서 버튼 누름"
+        }
+
     }
 
 }
